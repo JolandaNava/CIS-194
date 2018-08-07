83415543aeaf7ccd45271ffe030c801a07591906
Jolanda <ad11jnav@uwcad.it>
2018-08-07 10:17:03 +0800

    wip

--- src/Week4/Four.hs
+++ src/Week4/Four.hs
@@ -1,5 +1,7 @@
 module Week4.Four where
 
+import Data.Bits as Bits
+
 ---------- ex 1 ----------
 
 fun1' :: [Integer] -> Integer
@@ -9,4 +11,25 @@ fun2' :: Integer -> Integer
 fun2' = sum . filter even . takeWhile (>1) . iterate (\n -> if (even n) then (div n 2) else (3 * n + 1))
 
 ---------- ex 2 ----------
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+     deriving (Show, Eq)
+
+---------- ex 3.1 ----------
+
+xor' :: [Bool] -> Bool
+xor' = foldr Bits.xor False
+
+---------- ex 3.2 ----------
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\a -> (++) [f a] ) []
+
+---------- ex 3.3 ----------
+
+-- myFoldl :: (a -> b -> a) -> a -> [b] -> a
+-- myFoldl f base xs = foldr ...
+
+---------- ex 4 ----------
 

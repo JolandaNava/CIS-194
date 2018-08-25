08ac0a4c2020b8c58dd5f1fdaadb5f07dbdf5705
Jolanda <ad11jnav@uwcad.it>
2018-08-25 22:07:12 +0200

    hw week11 (#10)
    * hw week11
    
    * changed after review

---
+++ src/Week11/SExpr.hs
@@ -0,0 +1,57 @@
+module Week11.SExpr where
+
+import Control.Applicative
+import Data.Char
+import Week11.AParser
+
+------------------------------------------------------------
+--  1. Parsing repetitions
+------------------------------------------------------------
+
+zeroOrMore :: Parser a -> Parser [a]
+zeroOrMore p = oneOrMore p <|> pure []
+
+oneOrMore :: Parser a -> Parser [a]
+oneOrMore p = liftA2 (:) p $ zeroOrMore p
+
+------------------------------------------------------------
+--  2. Utilities
+------------------------------------------------------------
+
+spaces :: Parser String
+spaces = zeroOrMore $ satisfy isSpace
+
+ident :: Parser String
+ident = liftA2 (:) (satisfy isAlpha) (zeroOrMore $ satisfy isAlphaNum)
+
+------------------------------------------------------------
+--  3. Parsing S-expressions
+------------------------------------------------------------
+-- definitions
+type Ident = String
+
+data Atom = N Integer | I Ident
+  deriving Show
+
+data SExpr = A Atom
+           | Comb [SExpr]
+  deriving Show
+------------------------------------------------------------
+
+parseSExpr :: Parser SExpr
+parseSExpr = atomExpr <|> combExpr
+
+combExpr :: Parser SExpr
+combExpr = Comb <$> (open *> oneOrMore parseSExpr) <* close
+  where
+    open = removeSpaces $ char '('
+    close = removeSpaces $ char ')'
+
+atomExpr :: Parser SExpr
+atomExpr = removeSpaces $ A <$> (atomIdent <|> atomInt)
+  where
+    atomIdent =  I <$> ident
+    atomInt = N <$> posInt
+
+removeSpaces :: Parser a -> Parser a
+removeSpaces p = spaces *> p <* spaces
